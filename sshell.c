a5cb6e18eebe08465d3c3d7131e4e9ab617e6da8
joseph6s <75140405+joseph6s@users.noreply.github.com>
2022-01-15 16:07:01 -0800

    Update sshell.c

--- sshell.c
+++ sshell.c
@@ -2,8 +2,56 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <sys/wait.h>
 #define CMDLINE_MAX 512
+
+int sys_call(char cmd[])
+{
+        // make a copy of cmd
+        char cmd_copy[CMDLINE_MAX];
+        strcpy(cmd_copy, cmd);
+
+        /* split command pushed from user to */
+        char *token;
+        token = strtok(cmd_copy, " ");
+        
+        char* argu_array[17]; // need to be released
+        int i = 0;
+
+        while( token != NULL ) {
+                argu_array[i] = token;
+                token = strtok(NULL, " ");
+                i++;
+        }
+        argu_array[i] = NULL; // add a NULL in the end of the argument array => char *args[] = {"date", NULL} 
+        
+
+        /* fork + exec + wait */
+        pid_t pid;
+                
+
+        pid = fork();
+        if (pid == 0) {
+                /* Child process
+                end after execution */
+                execvp(cmd_copy, argu_array); 
+                perror("execvp");
+                exit(1);
+        } else if (pid > 0) {
+                /* Parent process
+                wait for Child
+                print finish statement
+                */
+                int status;
+                waitpid(pid, &status, 0);
+                fprintf(stderr, "+ completed '%s' [%d]\n",
+                        cmd, WEXITSTATUS(status));
+        } else {
+                perror("fork");
+                exit(1);
+        }
+        return 0;
+}
 
 int main(void)
 {
@@ -11,7 +59,7 @@ int main(void)
 
         while (1) {
                 char *nl;
-                int retval;
+                //int retval;
 
                 /* Print prompt */
                 printf("sshell$ ");
@@ -37,10 +85,8 @@ int main(void)
                         break;
                 }
 
-                /* Regular command */
-                retval = system(cmd); // too high level
-                fprintf(stdout, "Return status value for '%s': %d\n",
-                        cmd, retval);
+                // move the whole session of fork+wait+exec into helper function sys_call
+                sys_call(cmd); // require modified
         }
 
         return EXIT_SUCCESS;
