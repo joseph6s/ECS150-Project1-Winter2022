d753c4c27ed5fa4e7811c63766a6b8681ed07955
ShiweiHe137 <Alphe@ucdavis.edu>
2022-01-18 20:40:43 -0800

    Update sshell.c

--- sshell.c
+++ sshell.c
@@ -20,98 +20,111 @@ int redirection_detect(char* argu) // whether ">" is appeared in the command lin
 int sys_call(char cmd[])
 {
         // make a copy of cmd
-                        char cmd_copy[CMDLINE_MAX];
-                strcpy(cmd_copy, cmd);
-                char *token;
-                char* redirection_array[17];
-                char* argu_array[17]; // need to be released
-                
-                // phase 4
-                // echo a>test.txt // dup2 recover problem // do not stdout sshell
-                // echo a > test.txt // '\0' problem
-                // need a helper function to split the char[] ?
-                // release all temp variable, for next command
-
-                // redirction stdout to specfic file
-                int fd;
-                int redir_index = 0;
-                int i = 0;
-                if (redirection_detect(cmd_copy)){
-                        // resplit the argu_array
-                        // make an argu_array copy
-                        token = strtok(cmd_copy, ">");
-                        
-
-                        while( token != NULL ) {
-                                redirection_array[i] = token;
-                                token = strtok(NULL, ">");
-                                i++;
-                        }
-                        
-                        // example cmd : echo a b c > test.txt
-                        // split with >, get an array of ["echo a b c", "test.txt"]
-                        // take array[0] as new cmd, array[1] as redirection
-
-                        int j = 0;
-                        printf("r_array: ");
-                        while(j < i) {
-                                printf("%s\n",redirection_array[j]);
-                                j++;
-                        }
-                        printf("\n");
-
-                        // char* new_argu_array = redirection_array[0];
-                        printf("new_argu_array: %s\n", redirection_array[0]);
-                        printf("r_array[i]: %s\n", redirection_array[i-1]);
-
-                        // empty the token
-                        token = NULL;
-                        token = strtok(redirection_array[0], " ");
-                        i = 0;
-                        while( token != NULL ) {
-                                argu_array[i] = token;
-                                token = strtok(NULL, " ");
-                                i++;
-                        }
-
-                        j = 0;
-                        printf("argu_array: ");
-                        while(j < i) {
-                                printf("%s\n",argu_array[j]);
-                                j++;
-                        }
-                        printf("\n");
-                        redir_index = 1;
-
-                        // argu_array[i-1] = NULL;
-                        // argu_array[i-2] = NULL;
-                } else {
-                        // split command pushed from user
-                        token = strtok(cmd_copy, " ");
-                        while( token != NULL ) {
-                                argu_array[i] = token;
-                                token = strtok(NULL, " ");
-                                i++;
-                        }
-                        argu_array[i] = NULL; // add a NULL in the end of the argument array => char *args[] = {"date", NULL} 
-                }
-
-                // Build-in function pwd and cd
-                if (!strcmp(cmd_copy, "pwd")) {
-                        char cwd[PATH_MAX];
-                        getcwd(cwd,sizeof(cwd));
-                        fprintf(stdout, "%s\n",cwd);
-                        fprintf(stderr, "+ completed '%s' [%d]\n",
-                                cmd, 0);
-                        return 0;
-                } 
-                if (!strcmp(cmd_copy, "cd")) {                     
-                        int rc = chdir(argu_array[1]);
-                        fprintf(stderr, "+ completed '%s' [%d]\n",
-                                cmd, rc);
-                        return 0;      
-                }
+        char cmd_copy[CMDLINE_MAX];
+        strcpy(cmd_copy, cmd);
+        char *token;
+        char* redirection_array[17];
+        char* argu_array[17]; // need to be released
         
+        // phase 4
+        // echo a>test.txt // dup2 recover problem // do not stdout sshell
+        // echo a > test.txt // '\0' problem
+        // need a helper function to split the char[] ?
+        // release all temp variable, for next command
+
+        // redirction stdout to specfic file
+        int fd;
+        int redir_index = 0;
+        int i = 0;
+        if (redirection_detect(cmd_copy)){
+                // resplit the argu_array
+                // make an argu_array copy
+                token = strtok(cmd_copy, ">");
+                
+
+                while( token != NULL ) {
+                        redirection_array[i] = token;
+                        token = strtok(NULL, ">");
+                        i++;
+                }
+                
+                // delete the space in the name of file
+                if(strstr(redirection_array[i-1], " ")){
+                        token = NULL;
+                        token = strtok(redirection_array[i-1], " ");
+                        redirection_array[i-1] = token;
+                }
+
+                // example cmd : echo a b c > test.txt
+                // split with >, get an array of ["echo a b c", "test.txt"]
+                // take array[0] as new cmd, array[1] as redirection
+
+                /*
+                int j = 0;
+                printf("r_array: ");
+                while(j < i) {
+                        printf("%s\n",redirection_array[j]);
+                        j++;
+                }
+                printf("\n");
+
+                // char* new_argu_array = redirection_array[0];
+                printf("new_argu_array: %s\n", redirection_array[0]);
+                printf("r_array[i]: %s\n", redirection_array[i-1]);
+                */
+
+                // empty the token
+                token = NULL;
+                token = strtok(redirection_array[0], " ");
+                int j = 0;
+                while( token != NULL ) {
+                        argu_array[j] = token;
+                        token = strtok(NULL, " ");
+                        j++;
+                }
+                argu_array[j] = NULL;
+
+                /*
+                j = 0;
+                printf("argu_array: ");
+                while(j < i) {
+                        printf("%s\n",argu_array[j]);
+                        j++;
+                }
+                printf("\n"); 
+                */
+
+                redir_index = 1;
+
+                // argu_array[i-1] = NULL;
+                // argu_array[i-2] = NULL;
+        } else {
+                // split command pushed from user
+                token = strtok(cmd_copy, " ");
+                while( token != NULL ) {
+                        argu_array[i] = token;
+                        token = strtok(NULL, " ");
+                        i++;
+                }
+                argu_array[i] = NULL; // add a NULL in the end of the argument array => char *args[] = {"date", NULL} 
+        }
+
+        // Build-in function pwd and cd
+        if (!strcmp(cmd_copy, "pwd")) {
+                char cwd[PATH_MAX];
+                getcwd(cwd,sizeof(cwd));
+                fprintf(stdout, "%s\n",cwd);
+                fprintf(stderr, "+ completed '%s' [%d]\n",
+                        cmd, 0);
+                return 0;
+        } 
+        if (!strcmp(cmd_copy, "cd")) {                     
+                int rc = chdir(argu_array[1]);
+                fprintf(stderr, "+ completed '%s' [%d]\n",
+                        cmd, rc);
+                return 0;      
+        }
+
 
         /* fork + exec + wait */
         pid_t pid;     
