415c9ac8eda39ab22f862f56d13aa4f74deaba6e
ShiweiHe137 <Alphe@ucdavis.edu>
2022-01-20 17:51:13 -0800

    Update sshell.c

--- sshell.c
+++ sshell.c
@@ -9,6 +9,7 @@
 #define CMDLINE_MAX 512
 
 struct parse{
+        // a struct contained values
         int redir_index;
         int pipe_index;
         int cmd_len;
@@ -19,59 +20,47 @@ struct parse{
 };
 
 int sys_call(struct parse parse_rc,char* cmd);
-struct parse cmd_parse(char cmd[]);
 int stat(const char *filename, struct stat *buf);
-int fstat(int fd, struct stat *buf);
+struct parse cmd_parse(char cmd[]);
 
 int redirection_detect(char* argu) // whether ">" is appeared in the command line
 {    
-        if(strstr(argu, "&")){
+        if(strstr(argu, "&")) { // stderr case
                 return 2;
         }
-        else if (strstr(argu, ">")){
+        else if (strstr(argu, ">")) { // base case
                 return 1;
         }
         return 0;
 }
 
-int pipeline_detect(char* argu) // whether ">" is appeared in the command line
+int pipeline_detect(char* argu) // whether "|" is appeared in the command line
 {    
-        if(strstr(argu, "|")){
+        if(strstr(argu, "|")) {
                 return 1;
         }
         return 0;
 }
 
-void pipeline(struct parse parse_rc){
+void pipeline(struct parse parse_rc) // pipeline function
+{
+        // build a struct parse for store all values passed from commandline
         struct parse *parsed_cmd = (struct parse*)malloc(4*sizeof(struct parse));
-        //struct parse parsed_cmd1 = cmd_parse(parse_rc.pipe_arr[0]);
-        //printf("loop: %d, cmd: %s, and %s %s\n",12,parsed_cmd1.command,parsed_cmd1.argument[0],parsed_cmd1.argument[1]);
-        //printf("it issss %s\n",parse_rc.pipe_arr[0]);
-        //printf("count : %d\n",parse_rc.cmd_len);
-        for (int i = 0; i < parse_rc.cmd_len ; i++)
-        {
+        for (int i = 0; i < parse_rc.cmd_len ; i++) { // clean the space
                 parsed_cmd[i].argument = (char **) malloc(512*sizeof(char*));
                 parsed_cmd[i].command = (char *) malloc(CMDLINE_MAX*sizeof(char));
         }
-        for (int i = 0; i < parse_rc.cmd_len ; i++)
-        {
+        for (int i = 0; i < parse_rc.cmd_len ; i++) { // rebuild the argument array
                 parsed_cmd[i] = cmd_parse(parse_rc.pipe_arr[i]);
-                //printf("loop: %d, cmd: %s, and %s %s\n",i,parsed_cmd[i].command,parsed_cmd[i].argument[0],parsed_cmd[i].argument[1]);    
         }
         int i =0 ;
         int fd[2];
-        pid_t pid;
-        //printf("proce: 2, cmd: %s, and%sand%s\n",parsed_cmd[1].command,parsed_cmd[1].argument[0],parsed_cmd[1].argument[1]);           
-        for (; i < parse_rc.cmd_len-1  ; i++)
-        {
+        pid_t pid;        
+        for (; i < parse_rc.cmd_len-1; i++) {
                 pipe(fd);
-                //printf("11111111\n");
                 pid = fork(); 
                 if (pid==0) {
-                        //printf("22221\n");
-                        //printf("proce: 2, cmd:%s, and%sand%s\n",parse_rc.command,parse_rc.argument[0],parse_rc.argument[1]);
                         dup2(fd[1], STDOUT_FILENO);
-                        
                         execvp(parsed_cmd[i].command ,parsed_cmd[i].argument);
                         perror("execvp");
                 }
@@ -80,32 +69,28 @@ void pipeline(struct parse parse_rc){
         }
         int status;
         waitpid(pid, &status, 0);
-        //printf("33333\n");
-        //printf("proce: 2, cmd:%s, and%sand%s\n",parsed_cmd[i].command,parsed_cmd[i].argument[0],parsed_cmd[i].argument[1]);
         execvp(parsed_cmd[i].command ,parsed_cmd[i].argument);
         perror("execvp");
-
 }
 
-
-struct parse cmd_parse(char cmd[]) {
+struct parse cmd_parse(char cmd[]) // process value passed into the struct and assign them to correct variable
+{
         struct parse parse_input;       
         // make a copy of cmd
         char cmd_copy[CMDLINE_MAX];
         strcpy(cmd_copy, cmd);
-        char *token;
+        char *token; // used for strtok
         char* redirection_array[17];
-        char* argu_array[17]; // need to be released
+        char* argu_array[17];
         char* pipe_array[17];
-
-        // redirction stdout to specfic file
-        parse_input.cmd_len = 0;
-        int redir_index = 0;
+        char* cmd_rc = (char *) malloc(CMDLINE_MAX*sizeof(char));  
         int i = 0;
         int j = 0;
         int agrument_len = 0;
-        char* cmd_rc = (char *) malloc(CMDLINE_MAX*sizeof(char));
-        int pipe_index = pipeline_detect(cmd);
+        parse_input.cmd_len = 0;
+
+        int pipe_index = pipeline_detect(cmd_copy);
+        int redir_index = redirection_detect(cmd_copy);
         if (pipe_index) {
                 token = strtok(cmd_copy, "|");
                 while( token != NULL ) {
@@ -114,7 +99,7 @@ struct parse cmd_parse(char cmd[]) {
                         i++;
                 }
                 parse_input.cmd_len = i;        
-        } else if (redirection_detect(cmd_copy) == 1){
+        } else if (redir_index == 1) {
                 // resplit the argu_array
                 // make an argu_array copy
                 token = strtok(cmd_copy, ">");
@@ -124,8 +109,8 @@ struct parse cmd_parse(char cmd[]) {
                         i++;
                 }
                 
-                // delete the space in the name of file
-                if(strstr(redirection_array[i-1], " ")){
+                // delete the space in the filename
+                if(strstr(redirection_array[i-1], " ")) {
                         token = NULL;
                         token = strtok(redirection_array[i-1], " ");
                         redirection_array[i-1] = token;
@@ -134,7 +119,6 @@ struct parse cmd_parse(char cmd[]) {
                 // empty the token
                 token = NULL;
                 token = strtok(redirection_array[0], " ");
-                
                 while( token != NULL ) {
                         argu_array[j] = token;
                         token = strtok(NULL, " ");
@@ -143,9 +127,8 @@ struct parse cmd_parse(char cmd[]) {
                 cmd_rc = argu_array[0];
                 argu_array[j] = NULL;
                 agrument_len = j;
-                redir_index = 1;
         }
-        else if (redirection_detect(cmd_copy) == 2){
+        else if (redir_index == 2) {
                 // resplit the argu_array
                 // make an argu_array copy
                 token = strtok(cmd_copy, ">&");
@@ -155,8 +138,8 @@ struct parse cmd_parse(char cmd[]) {
                         i++;
                 }
                 
-                // delete the space in the name of file
-                if(strstr(redirection_array[i-1], " ")){
+                // delete the space in the filename
+                if(strstr(redirection_array[i-1], " ")) {
                         token = NULL;
                         token = strtok(redirection_array[i-1], " ");
                         redirection_array[i-1] = token;
@@ -165,7 +148,6 @@ struct parse cmd_parse(char cmd[]) {
                 // empty the token
                 token = NULL;
                 token = strtok(redirection_array[0], " ");
-                
                 while( token != NULL ) {
                         argu_array[j] = token;
                         token = strtok(NULL, " ");
@@ -174,7 +156,6 @@ struct parse cmd_parse(char cmd[]) {
                 cmd_rc = argu_array[0];
                 argu_array[j] = NULL;
                 agrument_len = j;
-                redir_index = 2;
         } else {
                 // split command pushed from user
                 token = strtok(cmd_copy, " ");
@@ -191,35 +172,24 @@ struct parse cmd_parse(char cmd[]) {
         parse_input.argument = (char **) malloc(CMDLINE_MAX*sizeof(char*));
         parse_input.command = (char *) malloc(CMDLINE_MAX*sizeof(char));
         parse_input.pipe_arr = (char **) malloc(CMDLINE_MAX*sizeof(char*));
-        //printf("=========arr len %d\n",agrument_len);
-        //printf("======== =cmd len %d\n",parse_input.cmd_len);
         parse_input.pipe_index = pipe_index;
         parse_input.redir_index = redir_index;
         strcpy(parse_input.command, cmd_rc);
-        for (int x = 0; x < parse_input.cmd_len ; x++)
-        {
+        for (int x = 0; x < parse_input.cmd_len ; x++) {
                 parse_input.pipe_arr[x] = (char *) malloc(CMDLINE_MAX*sizeof(char));
                 strcpy(parse_input.pipe_arr[x],pipe_array[x]);
         }
-        for (int x = 0; x < agrument_len ; x++)
-        {
-                //printf("=========arr len\n");
+        for (int x = 0; x < agrument_len ; x++) {
                 parse_input.argument[x] = (char *) malloc(CMDLINE_MAX*sizeof(char));
-                //printf("=========1111arr len\n");
                 strcpy(parse_input.argument[x],argu_array[x]);
         }
         parse_input.redir_arr = redirection_array[i-1];
-        //printf("proce: =====, cmd:%s, and%sand%s\n",parse_input.command,parse_input.argument[0],parse_input.argument[1]);
         return parse_input;
-
 }
 
-
-
-int sys_call(struct parse parse_rc,char*cmd)
+int sys_call(struct parse parse_rc,char*cmd) // defined sys_call function, include built-in function, fork + execute + wait
 {
-        int fd;
-        // Build-in function pwd and cd
+        // Build-in function pwd, cd and sls
         if (!strcmp(parse_rc.command, "pwd")) {
                 char cwd[PATH_MAX];
                 getcwd(cwd,sizeof(cwd));
@@ -234,55 +204,48 @@ int sys_call(struct parse parse_rc,char*cmd)
                         cmd,rc);
                 return 0;      
         }
-        // Build-in function sls
         if (!strcmp(parse_rc.command, "sls")) {                     
                 char cwd[PATH_MAX];
                 getcwd(cwd,sizeof(cwd)); // get current working directory
                 DIR *streamp;
                 struct dirent *dep;
                 struct stat s;
-                if (!(streamp = opendir(cwd))){
+                if (!(streamp = opendir(cwd))) {
                         exit(1);
                 }
-                while((dep = readdir(streamp)) != NULL){
+                while((dep = readdir(streamp)) != NULL) {
                         stat(dep->d_name, &s);
                         printf("%s (%ld bytes)\n", dep->d_name,s.st_size);
                 }
                 if (closedir(streamp)) {
                         exit(1);
                 }
-
                 fprintf(stderr, "+ completed '%s' [%d]\n",
                         cmd,0);
                 return 0;      
         }
 
         /* fork + exec + wait */
+        int fd;
         pid_t pid;     
         pid = fork();
         if (pid == 0) {
-                /* Child process
-                // end after execution */
-                        //printf("proce: 2, cmd:%s, and%sand%s\n",parse_rc.command,parse_rc.argument[0],parse_rc.argument[1]);
-                        //printf("================ %d \n",parse_rc.redir_index);
-                        if (parse_rc.redir_index == 1) {
+                // Child process end after execution
+                if (parse_rc.redir_index == 1) {
                         fd = open(parse_rc.redir_arr, O_WRONLY| O_CREAT,0644);
                         dup2(fd, STDOUT_FILENO);
                         close(fd);
-                        }
-                        else if (parse_rc.redir_index == 2){
+                }
+                else if (parse_rc.redir_index == 2) {
                         fd = open(parse_rc.redir_arr, O_WRONLY| O_CREAT,0644);
                         dup2(fd, STDERR_FILENO);
                         close(fd);        
-                        }
-                        execvp(parse_rc.command, parse_rc.argument);
-                        perror("execvp");
-                        exit(1);
+                }
+                execvp(parse_rc.command, parse_rc.argument);
+                perror("execvp");
+                exit(1);
         } else if (pid > 0) {
-                /* Parent process
-                wait for Child
-                print finish statement
-                */
+                // Parent process wait for Child print finish statement
                 int status;
                 waitpid(pid, &status, 0);
                 fprintf(stderr, "+ completed '%s' [%d]\n",
@@ -291,12 +254,10 @@ int sys_call(struct parse parse_rc,char*cmd)
                 perror("fork");
                 exit(1);
         }
-
-
         return 0;
 }
 
-int main(void)
+int main(void) 
 {
         char cmd[CMDLINE_MAX];
         while (1) {
@@ -318,18 +279,19 @@ int main(void)
 
                 /* Remove trailing newline from command line */
                 nl = strchr(cmd, '\n');
-                if (nl)
-                        *nl = '\0';
+                if (nl) {
+                        *nl = '\0';                        
+                }
 
-                /* Builtin command */
+                /* Builtin command exit */
                 if (!strcmp(cmd, "exit")) {
                         fprintf(stderr, "Bye...\n");
                         fprintf(stderr, "+ completed '%s' [%d]\n",
                                 cmd, 0);
                         break;
                 }
+
                 struct parse cmd_line = cmd_parse(cmd);
-                //printf("proce: 112121, cmd:%s, and%sand%s\n",cmd_line.command,cmd_line.argument[0],cmd_line.argument[1]);
                 // move the whole session of fork+wait+exec into helper function sys_call
                 if (cmd_line.pipe_index == 1){
                         pid_t pid;     
@@ -352,13 +314,9 @@ int main(void)
                                 perror("fork");
                                 exit(1);
                         }
-                        
-                }
-                else{
+                } else {
                         sys_call(cmd_line,cmd);
                 }
-                        
-
         }
         return EXIT_SUCCESS;
 }
